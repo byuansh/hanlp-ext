e2a67f6944424ad9659a5684cfba88075a14f1e2
niko.liu <niko.liu@vipshop.com>
2017-11-10 14:29:30 +0800

    修改支持es 5.6.3 增加了一个可配置的环境变量HANLP_PROPERTIES_PATH用于自定义词典

--- es-plugin/src/test/java/com/niko/hanlptest/TestHanlpLoadProp.java
+++ es-plugin/src/test/java/com/niko/hanlptest/TestHanlpLoadProp.java
@@ -3,22 +3,44 @@ package com.niko.hanlptest;
 import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.seg.common.Term;
 import com.hankcs.hanlp.utility.Predefine;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Properties;
 
 public class TestHanlpLoadProp {
 
     private Logger logger = LoggerFactory.getLogger(TestHanlpLoadProp.class);
 
-    @Test
+    @Ignore
     public void testLoadProp() {
         Predefine.HANLP_PROPERTIES_PATH = "D:\\hanlp.properties";
         List<Term> segs = HanLP.segment("重庆电商物流仓库人员");
         for (Term term : segs) {
             logger.info("term={}", term.toString());
+        }
+    }
+
+    @Test
+    public void testFile() {
+        SecurityManager securityManager = System.getSecurityManager();
+        if (securityManager == null) {
+
+        }
+        try (FileInputStream fin = new FileInputStream("d:\\hanlp.properties")) {
+            Properties properties = new Properties();
+            properties.load(fin);
+
+            String root = properties.getProperty("root");
+            logger.info("root={}", root);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
         }
     }
 }
